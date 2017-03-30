ab01b316c17dac9ef976dd3df61d1ee88fa5acf7
Aymeric Dujardin <aymeric.dujardin@stereolabs.com>
2017-03-30 10:56:00 +0200

    Update for ZED SDK 2.0

--- src/main.cpp
+++ src/main.cpp
@@ -23,156 +23,175 @@
  ** This sample demonstrates how to use PCL (Point Cloud Library) with the ZED SDK **
  ************************************************************************************/
 
-#include <stdio.h>
-#include <string.h>
-#include <ctime>
-#include <chrono>
-#include <thread>
-#include <mutex>
+// ZED includes
+#include <sl/Camera.hpp>
 
-#include <opencv2/opencv.hpp>
-
-#include <zed/Camera.hpp>
-#include <zed/utils/GlobalDefine.hpp>
-
+// PCL includes
+// Undef on Win32 min/max for PCL
 #ifdef _WIN32
 #undef max
 #undef min
 #endif
-
 #include <pcl/common/common_headers.h>
 #include <pcl/visualization/pcl_visualizer.h>
 
-using namespace sl::zed;
+// Sample includes
+#include <thread>
+#include <mutex>
+
+// Namespace
+using namespace sl;
 using namespace std;
 
-typedef struct image_bufferStruct {
-    float* data_cloud;
-    std::mutex mutex_input;
-    int width, height;
-} image_buffer;
-
-
-Camera* zed;
-image_buffer* buffer;
-SENSING_MODE dm_type = STANDARD;
+// Global instance (ZED, Mat, callback)
+Camera zed;
+Mat data_cloud;
+std::thread zed_callback;
+std::mutex mutex_input;
 bool stop_signal;
+bool has_data;
 
-// Grab called in a thread to parallelize the rendering and the computation
+// Sample functions
+void startZED();
+void run();
+void closeZED();
+shared_ptr<pcl::visualization::PCLVisualizer> createRGBVisualizer(pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr cloud);
+inline float convertColor(float colorIn);
 
-void grab_run() {
-    float* p_cloud;
+// Main process
 
+int main(int argc, char** argv) {
+
+    if (argc > 2) {
+        cout << "Only the path of a SVO can be passed in arg" << endl;
+        return -1;
+    }
+
+    // Set configuration parameters
+    InitParameters init_params;
+    init_params.camera_resolution = RESOLUTION_HD720;
+    if (argc == 2) init_params.svo_input_filename = argv[1];
+    init_params.coordinate_units = UNIT_METER;
+    init_params.coordinate_system = COORDINATE_SYSTEM_RIGHT_HANDED_Y_UP;
+    init_params.depth_mode = DEPTH_MODE_PERFORMANCE;
+    init_params.camera_fps = 30;
+
+    // Open the camera
+    ERROR_CODE err = zed.open(init_params);
+    if (err != SUCCESS) {
+        cout << errorCode2str(err) << endl;
+        return 1;
+    }
+
+    // Allocate PCL point cloud at the resolution
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr p_pcl_point_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+    p_pcl_point_cloud->points.resize(zed.getResolution().area());
+
+    // Create the PCL point cloud visualizer
+    shared_ptr<pcl::visualization::PCLVisualizer> viewer = createRGBVisualizer(p_pcl_point_cloud);
+
+    // Start ZED callback
+    startZED();
+
+    // Loop until viewer catches the stop signal
+    while (!viewer->wasStopped()) {
+
+        // Try to lock the data if possible (not in use). Otherwise, do nothing.
+        if (mutex_input.try_lock()) {
+            float *p_data_cloud = data_cloud.getPtr<float>();
+            int index = 0;
+
+            // Check and adjust points for PCL format
+            for (auto &it : p_pcl_point_cloud->points) {
+                float X = p_data_cloud[index];
+                if (!isValidMeasure(X)) // Checking if it's a valid point
+                    it.x = it.y = it.z = it.rgb = 0;
+                else {
+                    it.x = X;
+                    it.y = p_data_cloud[index + 1];
+                    it.z = p_data_cloud[index + 2];
+                    it.rgb = convertColor(p_data_cloud[index + 3]); // Convert a 32bits float into a pcl .rgb format
+                }
+                index += 4;
+            }
+
+            // Unlock data and update Point cloud
+            mutex_input.unlock();
+            viewer->updatePointCloud(p_pcl_point_cloud);
+            viewer->spinOnce(15);
+        } else
+            sleep_ms(1);
+    }
+
+    // Close the viewer
+    viewer->close();
+
+    // Close the zed
+    closeZED();
+
+    return 0;
+}
+
+/**
+ *  This functions start the ZED's thread that grab images and data.
+ **/
+void startZED() {
+    // Start the thread for grabbing ZED data
+    stop_signal = false;
+    has_data = false;
+    zed_callback = std::thread(run);
+
+    //Wait for data to be grabbed
+    while (!has_data)
+        sleep_ms(1);
+}
+
+/**
+ *  This function loops to get the point cloud from the ZED. It can be considered as a callback.
+ **/
+void run() {
     while (!stop_signal) {
-        if (!zed->grab(dm_type)) {
-            p_cloud = (float*) zed->retrieveMeasure(MEASURE::XYZRGBA).data; // Get the pointer
-            // Fill the buffer
-            buffer->mutex_input.lock(); // To prevent from data corruption
-            memcpy(buffer->data_cloud, p_cloud, buffer->width * buffer->height * sizeof (float) * 4);
-            buffer->mutex_input.unlock();
+        if (zed.grab(SENSING_MODE_STANDARD) == SUCCESS) {
+            mutex_input.lock(); // To prevent from data corruption
+            zed.retrieveMeasure(data_cloud, MEASURE_XYZRGBA);
+            mutex_input.unlock();
+            has_data = true;
         }
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        sleep_ms(1);
     }
 }
 
-std::shared_ptr<pcl::visualization::PCLVisualizer> rgbVis(pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr cloud) {
+/**
+ *  This function frees and close the ZED, its callback(thread) and the viewer
+ **/
+void closeZED() {
+    // Stop the thread
+    stop_signal = true;
+    zed_callback.join();
+    zed.close();
+}
+
+/**
+ *  This function creates a PCL visualizer
+ **/
+shared_ptr<pcl::visualization::PCLVisualizer> createRGBVisualizer(pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr cloud) {
     // Open 3D viewer and add point cloud
-    std::shared_ptr<pcl::visualization::PCLVisualizer> viewer(new pcl::visualization::PCLVisualizer("PCL ZED 3D Viewer"));
+    shared_ptr<pcl::visualization::PCLVisualizer> viewer(new pcl::visualization::PCLVisualizer("PCL ZED 3D Viewer"));
     viewer->setBackgroundColor(0.12, 0.12, 0.12);
     pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb(cloud);
-    viewer->addPointCloud<pcl::PointXYZRGB> (cloud, rgb);
+    viewer->addPointCloud<pcl::PointXYZRGB>(cloud, rgb);
     viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1.5);
     viewer->addCoordinateSystem(1.0);
     viewer->initCameraParameters();
     return (viewer);
 }
 
-int main(int argc, char** argv) {
-    stop_signal = false;
-
-    if (argc > 2) {
-        std::cout << "Only the path of a SVO can be passed in arg" << std::endl;
-        return -1;
-    }
-
-    if (argc == 1) // Live Mode
-        zed = new Camera(HD720);
-    else // SVO playback mode
-        zed = new Camera(argv[1]);
-
-    sl::zed::InitParams params;
-    params.mode = PERFORMANCE;
-    params.unit = METER; // Scale to fit OpenGL world
-    params.coordinate = RIGHT_HANDED; // OpenGL compatible
-    params.verbose = true;
-
-    ERRCODE err = zed->init(params);
-    cout << errcode2str(err) << endl;
-    if (err != SUCCESS) {
-        delete zed;
-        return 1;
-    }
-
-    int width = zed->getImageSize().width;
-    int height = zed->getImageSize().height;
-
-    // Allocate data
-    buffer = new image_buffer();
-    buffer->height = height;
-    buffer->width = width;
-    buffer->data_cloud = new float[buffer->height * buffer->width * 4];
-
-    int size = height*width;
-
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr point_cloud_ptr(new pcl::PointCloud<pcl::PointXYZRGB>);
-    std::shared_ptr<pcl::visualization::PCLVisualizer> viewer = rgbVis(point_cloud_ptr);
-
-    float* data_cloud;
-    // Run thread
-    std::thread grab_thread(grab_run);
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
-    float color;
-    int index4 = 0;
-
-    point_cloud_ptr->points.resize(size);
-
-    while (!viewer->wasStopped()) {
-
-        if (buffer->mutex_input.try_lock()) {
-            data_cloud = buffer->data_cloud;
-            index4 = 0;
-
-            for (auto &it : point_cloud_ptr->points) {
-                float X = data_cloud[index4 * 4];
-                if (!isValidMeasure(X)) // Checking if it's a valid point
-                    it.x = it.y = it.z = it.rgb = 0;
-                else {
-                    it.x = X;
-                    it.y = data_cloud[index4 * 4 + 1];
-                    it.z = data_cloud[index4 * 4 + 2];
-                    color = data_cloud[index4 * 4 + 3];
-                    // Color conversion (RGBA as float32 -> RGB as uint32)
-                    uint32_t color_uint = *(uint32_t*) & color;
-                    unsigned char* color_uchar = (unsigned char*) &color_uint;
-                    color_uint = ((uint32_t) color_uchar[0] << 16 | (uint32_t) color_uchar[1] << 8 | (uint32_t) color_uchar[2]);
-                    it.rgb = *reinterpret_cast<float*> (&color_uint);
-                }
-                index4++;
-            }
-            buffer->mutex_input.unlock();
-            viewer->updatePointCloud(point_cloud_ptr);
-            viewer->spinOnce(15);
-        }
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
-    }
-
-    // Stop the grabbing thread
-    stop_signal = true;
-    grab_thread.join();
-
-    delete[] buffer->data_cloud;
-    delete buffer;
-    delete zed;
-    return 0;
+/**
+ *  This function convert a RGBA color packed into a packed RGBA PCL compatible format
+ **/
+inline float convertColor(float colorIn) {
+    uint32_t color_uint = *(uint32_t*) & colorIn;
+    unsigned char* color_uchar = (unsigned char*) &color_uint;
+    color_uint = ((uint32_t) color_uchar[0] << 16 | (uint32_t) color_uchar[1] << 8 | (uint32_t) color_uchar[2]);
+    return *reinterpret_cast<float*> (&color_uint);
 }
